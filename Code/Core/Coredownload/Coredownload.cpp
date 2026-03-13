c21db89fe38633bc08fbd7ef0f58dac1be5563d7
duongdat.20061103@gmail.com <duongdat.20061103@gmail.com>
2026-03-13 20:57:27 +0700

    pussh

---
+++ Code/Core/Coredownload/Coredownload.cpp
@@ -0,0 +1,11 @@
+// Coredownload.cpp : Defines the functions for the static library.
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "Coredownload.h"
+
+// TODO: This is an example of a library function
+void fnCoredownload()
+{
+}
